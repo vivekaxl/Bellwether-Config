468f314772a5d8fd6309788216107f867e07a27f
Vivek <vivekaxl@gmail.com>
2018-02-16 01:57:39 -0500

    using different sqlite data

--- Data/temp.py
+++ Data/temp.py
@@ -4,4 +4,5 @@ files = [f for f in os.listdir('.') if '.csv' in f]
 
 for file in sorted(files):
     c = open(file).readline()
-    print file, len(c.split(','))-1
+    tc = open(file).readlines()
+    print file, len(c.split(','))-1, len(tc)-1
