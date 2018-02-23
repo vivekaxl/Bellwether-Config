3fae985d5efdb922a018c95968eef52b1558974a
Vivek Nair <vivekaxl@gmail.com>
2018-02-23 10:57:11 -0500

    tuning experiment

--- RQ4_exp/run.py
+++ RQ4_exp/run.py
@@ -11,7 +11,7 @@ from random import seed
 import copy
 
 
-def run(files, no_rows, no_columns):
+def run(files, no_rows, no_columns, percentage, loss):
     result_collector = {}
     contents = {}
     local_files = copy.copy(files)
@@ -25,7 +25,7 @@ def run(files, no_rows, no_columns):
         measurements[file] = []
 
     prev = sys.maxint
-    loosy=4
+    loosy=loss
     gen = 1
     while True:
         result_collector[gen] = {}
@@ -71,7 +71,7 @@ def run(files, no_rows, no_columns):
                 assert (len(ctarget_dep) == 1), "Something is wrong"
                 ctarget_dep = ctarget_dep[0]
 
-                target_content = target_content.sort_values(by=ctarget_dep)
+                target_content = target_content.sort(ctarget_dep)
                 target_indep = target_content[ctarget_indep]
                 target_dep = target_content[ctarget_dep]
                 # Predict the target from the source model
@@ -101,12 +101,56 @@ def run(files, no_rows, no_columns):
             loosy -= 1
         prev = min(prev, len(local_files))
 
-        if len(measurements[local_files[0]]) > no_rows * 0.08 or loosy == 0:
+        if len(measurements[local_files[0]]) > no_rows * percentage or loosy == 0:
             break
         gen += 1
     return local_files
 
-if __name__ == "__main__":
+def get_rd(family, detected_bws):
+    data_folder = '../Data/'
+    files = [data_folder + f for f in os.listdir(data_folder) if family in f ]
+    files = [f for f in files if os.path.isfile(f)]
+    ret_rd = []
+    for detected_bw in detected_bws:
+        rd_collector = []
+        for target in files:
+            if detected_bw == file: continue
+            train_content = pd.read_csv(detected_bw)
+            train_cols = train_content.columns.values.tolist()
+            ctrain_indep = [c for c in train_cols if '<$' not in c]
+            ctrain_dep = [c for c in train_cols if '<$' in c]
+            assert (len(ctrain_dep) == 1), "Something is wrong"
+
+            ctrain_dep = ctrain_dep[0]
+            train_indep = train_content[ctrain_indep]
+            train_dep = train_content[ctrain_dep]
+
+            # Train a model using source data
+            source_model = DecisionTreeRegressor()
+            source_model.fit(train_indep, train_dep)
+
+            # Read target data
+            target_content = pd.read_csv(target)
+            target_cols = target_content.columns.values.tolist()
+            ctarget_indep = [c for c in target_cols if '<$' not in c]
+            ctarget_dep = [c for c in target_cols if '<$' in c]
+            assert (len(ctarget_dep) == 1), "Something is wrong"
+            ctarget_dep = ctarget_dep[0]
+
+            test_indep = target_content[ctarget_indep]
+            test_dep = target_content[ctarget_dep]
+            # Take care of duplicate performance values
+            l_ranks = np.searchsorted(np.sort(test_dep), test_dep).tolist()
+
+            target_test_predict_dep = source_model.predict(test_indep)
+
+            ranks = [i[0] for i in sorted(enumerate(target_test_predict_dep), key=lambda x: x[1])]
+            rd_collector.append(l_ranks[ranks[0]])
+        ret_rd.append(np.median(rd_collector))
+    return ret_rd
+
+
+def run_main(step_size, percentage, loss):
     bellwethers = {
         'sac': ['sac_4'],
         'sqlite': ['sqlite_88'],
@@ -116,7 +160,7 @@ if __name__ == "__main__":
         'storm-obj2': ['storm-obj2_feature7']
     }
 
-    seed(10)
+    # seed(10)
     reps = 20
     familys = ['storm-obj1', 'storm-obj2','x264','spear', 'sac',    # 'sqlite',
                ]
@@ -139,15 +183,20 @@ if __name__ == "__main__":
         'storm-obj2': 2049,
     }
 
+    print '== ' * 20
+    print step_size, percentage, loss
+
     collector = {}
     for family in familys:
-        collector[family] = {}
+        collector[family] = []
         files = [data_folder + file for file in os.listdir(data_folder) if family in file]
         files = [f for f in files if os.path.isfile(f)]
         count = 0
-        for _ in xrange(20):
+        for _ in xrange(reps):
             print ". ",
-            ret = run(files, rows[family], 7)
+            ret = run(files, rows[family], step_size, percentage, loss)
+            ret_rd = get_rd(family, ret)
+            collector[family].append([ret, ret_rd])
             # print ret
             for bw in bellwethers[family]:
                 if '../Data/' + bw + '.csv' in ret:
@@ -157,4 +206,23 @@ if __name__ == "__main__":
 
         print
         print family, count
-        print '== ' * 20
+    print '== ' * 20
+
+    import pickle
+    pickle.dump(collector, open('./Pickle_Folder/experiment_pickle_' + str(step_size) + '_' + str(percentage)+ '_' + str(loss) + '.p', 'w'))
+
+if __name__ == '__main__':
+    step_sizes = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    percentages = [0.05, 0.07, 0.09, 0.1, 0.13, 0.15, 0.18, 0.2]
+    losses = [3, 4, 5, 6, 7,]
+    import multiprocessing as mp
+    # Main control loop
+    pool = mp.Pool()
+
+    for step_size in step_sizes:
+        for percentage in percentages:
+            for loss in losses:
+                pool.apply_async(run_main, (step_size, percentage, loss))
+
+    pool.close()
+    pool.join()
