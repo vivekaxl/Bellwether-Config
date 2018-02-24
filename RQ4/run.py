05edd55cbb1c629ef5b83e5ae2abab041150f744
Vivek <vivekaxl@gmail.com>
2018-02-24 16:25:56 -0500

    rerunning for sqlite

--- RQ4/run.py
+++ RQ4/run.py
@@ -78,10 +78,10 @@ def run(files, no_rows, no_columns):
 if __name__ == "__main__":
     seed(10)
     reps = 20
-    familys = [ 'spear', 'sac',  'x264', 'storm-obj1', 'storm-obj2', #'sqlite',
+    familys = [ 'spear', 'sac',  'x264', 'storm-obj1', 'storm-obj2', 'sqlite',
                ]
     data_folder = "../Data/"
-    training_percents = [1, 2, 3, 4, 5, 6,]# 7, 8, 9, 10, ]#11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    training_percents = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ]#11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     columns = {
         'sac': 57,
         'sqlite': 14,
